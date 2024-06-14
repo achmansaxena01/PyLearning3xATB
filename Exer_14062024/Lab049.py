64f84554b1d608a9c8011a9936edb30dff63e8a5
Achman Saxena <avachmansaxena@gmail.com>
2024-06-14 07:18:21 +0530

    push96

---
+++ Exer_14062024/Lab049.py
@@ -0,0 +1,10 @@
+# for loop
+
+for counter in range(1,101,2):
+    print(counter)
+for counter1 in range(1,101):
+    print(counter1)
+    if counter1 == 5:
+        break
+
+print("outside of the program")
