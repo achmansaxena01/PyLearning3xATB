8bed354d2a50dd7122d20ef6c78c0e711646351e
0033 <avachmansaxena@gmail.com>
2024-06-28 08:24:08 +0530

    commit office1

--- Exer_26062024/Lab114.py
+++ Exer_26062024/Lab114.py
@@ -12,13 +12,14 @@ class Dog:
 
 dog1 = Dog("Chow", "1")
 dog2 = Dog("Mow","2")
-dog3 = Dog
+dog3 = Dog()
 dog3.name = "now"
 dog3.id = "3"
 
 print(f'{dog1.name} has ID {dog1.id}')
 print(f'{dog2.name} has ID {dog2.id}')
 print(f'{dog3.name} has ID {dog3.id}')
+print("https://github.com/PramodDutta/Py3xATBLearning")
 
 dog1.sleep()
 dog2.sleep()
